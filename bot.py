2bce3b59bba17b38e6bf2c3dd4b68c0e1183de95
omznc <38432561+omznc@users.noreply.github.com>
2021-03-16 18:35:29 +0100

    Author Icons

--- bot.py
+++ bot.py
@@ -7,13 +7,22 @@ import time
 import json
 import os
 
-# Settings.
-parseHubKey = "parsehub_key_here" 
-parseHubLink_run = 'link_here'
-parseHubLink_fetch = 'link_here'
-botToken = 'bot_token_here'
-thumbnailURL = "raw_image_url_here"
-sendChannelID = 000
+# Settings
+parseHubKey = "your key" 
+parseHubLink_run = 'url'
+parseHubLink_fetch = 'url'
+botToken = 'token'
+thumbnailURL = "https://i.imgur.com/2c6X56V.png"
+channelID = channel-id 
+
+# Author Icons
+default = "https://i.imgur.com/cmXAse2.jpeg"
+irisMemic = "https://i.imgur.com/P3tNbI6.jpg"
+senadRahimic = "https://i.imgur.com/RC1Cthj.jpeg"
+edinaCmanjcanin = "https://i.imgur.com/gZ3DWaU.png"
+elmirBabovic = "https://i.imgur.com/KvsbWXn.png"
+denisMusic = "https://i.imgur.com/WQH2OJh.png"
+indiraHamulic = "https://i.imgur.com/oZ1N2vv.png"
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
@@ -35,15 +44,16 @@ class MyClient(discord.Client):
         requests.post(parseHubLink_run, params=params)
         await asyncio.sleep(30)
         r = requests.get(parseHubLink_fetch, params=params)
-        data = json.loads(r.text)
+        data = json.loads(r.text)  
+        msg = await self.get_channel(channelID).history(limit=1).flatten()
+        msg = msg[0]
+        embeds = msg.embeds
+        for embed in embeds:
+            old_embed = embed.to_dict()
+        channel = self.get_channel(channelID)
 
-        channel = self.get_channel(sendChannelID)
-        temp = await channel.history(limit=1).flatten()
-        temp = temp[0]
-        date = data["date"]
-
-        # Compares the post date to see if it's the same post. I know... not a great solution.
-        if date[:-2] not in temp.content:
+        # Checks if it's the same post. If no, proceed.
+        if old_embed["title"] != data["title"]:
             if "content" not in data.keys():
                 noPostDescription = "Obavijest nema teksta. Kliknite na naslov da otvorite u browser-u."
                 embed=discord.Embed(title=data["title"], url=data["article_url"], description=noPostDescription, color=0xf6f6f6)
@@ -56,15 +66,32 @@ class MyClient(discord.Client):
             else:
                 embed=discord.Embed(title=data["title"], url=data["article_url"], description=data["content"], color=0xf6f6f6)
 
-            embed.set_author(name=data["author"])
-            embed.set_footer(text="www.omarzunic.com")
+            # Please don't look at this code.
+            if "Iris" in data["author"]:
+                icon = irisMemic
+            elif "Senad" in data["author"]:
+                icon = senadRahimic
+            elif "Edina" in data["author"]:
+                icon = edinaCmanjcanin
+            elif "Elmir" in data["author"]:
+                icon = elmirBabovic
+            elif "Denis" in data["author"]:
+                icon = denisMusic
+            elif "Indira" in data["author"]:
+                icon = indiraHamulic
+            else:
+                 icon = default
+                
+            date = data["date"]
+            embed.set_author(name=data["author"], icon_url=icon)
+            embed.set_footer(text=f"Posted on {date[:-1]} |  github.com/omznc/discord-scraper")
             embed.set_thumbnail(url=thumbnailURL)
             
-            await channel.send(f"Objavljeno **{date[:-2]}**", embed=embed)
+            await channel.send("<@&796116996000579644>", embed=embed)
         
     @background.before_loop
     async def before_loop(self):
-        print("Waiting until the bot initializes...")
+        print("Waiting until the bot starts...")
         await self.wait_until_ready()
 
 client = MyClient()
